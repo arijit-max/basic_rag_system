feb8fdf463574eb19f0a843320a63569bd16abe0
Arijit Barua <barua94a@gmail.com>
2026-08-19 13:02:31 +0530

    Added LLM response

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -1,8 +1,12 @@
+import os
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 
 load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 persist_directory = "vector_store"
 
@@ -32,8 +36,30 @@ for i, doc in enumerate(relevant_docs):
     print(doc.page_content)
     print(doc.metadata)
 
+context = "\n\n".join(doc.page_content for doc in relevant_docs)
+combined_input = f"""Based on the following context, answer the question: {query}
 
+Context:
+{context}
 
+Please provide a concise and accurate answer to the question based on the context provided. If the context does not contain enough information to answer the question, please indicate that as well.
+"""
+client = InferenceClient(
+    model="Qwen/Qwen3-8B",
+    token=HF_TOKEN
+)
 
+messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant that provides accurate and concise answers based on the provided context.",
+    },
+    {"role": "user", "content": combined_input},
+]
 
-
+result = client.chat.completions.create(
+    model="Qwen/Qwen3-8B",
+    messages=messages
+)
+print("---Answer---")
+print(result.choices[0].message.content)
